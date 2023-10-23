8c50bfe2d79ab3a8f79fc997eaf1ae62acadd98f
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2023-10-23 22:02:12 +0200

    Add contract form

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <div
-        id="HOMERoot"
         className="overflow-hidden bg-[linear-gradient(349deg,_#4a056a_-9%,#09092e_16%,rgba(0,_6,_23,_0)_56%),_linear-gradient(164deg,_#12056a_-14%,#020a07_21%,#06012e_54%)] bg-cover,_cover bg-50%_50%,_50%_50% bg-blend-normal,_normal bg-repeat-no-repeat,_no-repeat flex flex-col justify-between gap-[430px] w-full"
       >
         <div className="flex flex-col gap-[257px]">
--- components/Works/Works.tsx
+++ components/Works/Works.tsx
@@ -1,7 +1,7 @@
 const HomeWorks = () =>{
     return(
         <div className="flex flex-col gap-20 items-center mb-8 mx-[171px]">
-            <div className="flex flex-col gap-5 w-3/5 items-center">
+            <div className="mx-auto flex max-w-xl flex-col items-center text-center">
               <div
                 id="OurWorks"
                 className="text-center text-4xl font-['DM_Sans'] font-bold tracking-[-0.4] leading-[52px] text-white"
@@ -18,112 +18,10 @@ const HomeWorks = () =>{
               </div>
             </div>
             <div className="self-stretch flex flex-col gap-12">
-              <div className="flex flex-row justify-between items-start">
-                <div className="flex flex-col gap-5 items-center">
-                  <img src="" id="Mask4" className="self-start" />
-                  <div className="flex flex-col gap-3 items-start">
-                    <div
-                      id="InTheSky"
-                      className="text-center text-xl font-['DM_Sans'] font-medium leading-[32px] text-white ml-px"
-                    >
-                      Nexarb SOL
-                    </div>
-                    <div
-                      id="NicoleMilton"
-                      className="text-center text-sm font-['DM_Sans'] font-bold tracking-[2] leading-[26px] uppercase text-white"
-                    >
-                      october 2023
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-5 items-center">
-                  <img src="" id="Mask5" className="self-start" />
-                  <div className="flex flex-col gap-3 items-center">
-                    <div
-                      id="InTheSky1"
-                      className="text-center text-xl font-['DM_Sans'] font-medium leading-[32px] text-white self-start"
-                    >
-                      PROJECT 2
-                    </div>
-                    <div
-                      id="NicoleMilton1"
-                      className="text-center text-sm font-['DM_Sans'] font-bold tracking-[2] leading-[26px] uppercase text-white"
-                    >
-                      2024
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-5 items-center">
-                  <img src="" id="Mask6" className="self-start" />
-                  <div className="flex flex-col gap-3 items-center">
-                    <div
-                      id="InTheSky2"
-                      className="text-center text-xl font-['DM_Sans'] font-medium leading-[32px] text-white self-start"
-                    >
-                      PROJECT 3
-                    </div>
-                    <div
-                      id="NicoleMilton2"
-                      className="text-center text-sm font-['DM_Sans'] font-bold tracking-[2] leading-[26px] uppercase text-white"
-                    >
-                      2024
-                    </div>
-                  </div>
-                </div>
+              <div className="mx-auto flex max-w-xl flex-col items-center text-center">
+                Video
               </div>
-              <div className="flex flex-row justify-between items-start">
-                <div className="flex flex-col gap-5 items-center">
-                  <img src="" id="Mask7" className="self-start" />
-                  <div className="flex flex-col gap-3 items-center">
-                    <div
-                      id="InTheSky3"
-                      className="text-center text-xl font-['DM_Sans'] font-medium leading-[32px] text-white self-start"
-                    >
-                      PROJECT 4
-                    </div>
-                    <div
-                      id="NicoleMilton3"
-                      className="text-center text-sm font-['DM_Sans'] font-bold tracking-[2] leading-[26px] uppercase text-white"
-                    >
-                      2024
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-5 items-center">
-                  <img src="" id="Mask8" className="self-start" />
-                  <div className="flex flex-col gap-3 items-center">
-                    <div
-                      id="InTheSky4"
-                      className="text-center text-xl font-['DM_Sans'] font-medium leading-[32px] text-white self-start"
-                    >
-                      PROJECT 5
-                    </div>
-                    <div
-                      id="NicoleMilton4"
-                      className="text-center text-sm font-['DM_Sans'] font-bold tracking-[2] leading-[26px] uppercase text-white"
-                    >
-                      2025
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-5 items-center">
-                  <img src="" id="Mask9" className="self-start" />
-                  <div className="flex flex-col gap-3 items-center">
-                    <div
-                      id="InTheSky5"
-                      className="text-center text-xl font-['DM_Sans'] font-medium leading-[32px] text-white self-start"
-                    >
-                      PROJECT 6
-                    </div>
-                    <div
-                      id="NicoleMilton5"
-                      className="text-center text-sm font-['DM_Sans'] font-bold tracking-[2] leading-[26px] uppercase text-white"
-                    >
-                      2026
-                    </div>
-                  </div>
-                </div>
-              </div>
+              
             </div>
           </div>
     )
